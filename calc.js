73a3321c94ec54b3b6414f27066c3f4a68e23c40
Dikshij <dikshi.01@gmail.com>
2022-03-21 21:36:22 +0530

    2 files modified

--- calc.js
+++ calc.js
@@ -2,9 +2,18 @@ function add(a, b) {
   return a + b;
 }
 
+function sub(a, b) {
+  return a - b;
+}
+
+exports.mul = function multiply(a, b) {
+  return a * b;
+};
+
 //way 1
 //module.exports = add;
 
 //way2
 
 module.exports.add = add;
+exports.sub = sub;
--- module_eg.js
+++ module_eg.js
@@ -3,7 +3,11 @@ var calc = require('./calc.js');
 //result = calc(3,4)
 //way 2
 result = calc.add(3, 4);
+result1 = calc.sub(3, 4);
+result2 = calc.mul(3, 4);
 
 console.log(typeof result);
 
 console.log('sun is' + result);
+console.log('difference is' + result1);
+console.log('multiplication is' + result2);
