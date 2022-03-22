09b803fc69e0d37ed1312f1dc99bfd580c33cafb
Dikshij <dikshi.01@gmail.com>
2022-03-22 08:43:51 +0530

    1 file modified, 1 deleted

--- file_system.js
+++ file_system.js
@@ -7,3 +7,12 @@ fs.readFile('calc.js', 'utf-8', function (err, data) {
 fs.writeFile('custom.js', 'console.log("hiii")', function (err) {
   console.log('completd');
 });
+
+fs.appendFile('custom.js', 'console.log("hiiihhhh")', function (err) {
+  console.log('completd');
+});
+
+//to deletr a file
+fs.unlink('custom.js', function (err) {
+  console.log('completd');
+});
