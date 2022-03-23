9f48edb4cd29270ba3f551021f1ae0e0d3cf354e
Dikshij <dikshi.01@gmail.com>
2022-03-23 06:32:41 +0530

    1 file modified

--- express_eg.js
+++ express_eg.js
@@ -1,3 +1,13 @@
 const express = require('express');
 
 const app = express();
+
+//to start a Server
+app.listen(9000, function (err, res) {
+  console.log('Running');
+});
+
+//get api
+app.get('/', function (req, res) {
+  res.send('Heyy');
+});
