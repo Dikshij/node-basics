e4756aecdf956e43b2e0367f7f46c736168540ba
Dikshij <dikshi.01@gmail.com>
2022-03-23 07:13:17 +0530

    1 file added

---
+++ userInput.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+
+  output: process.stdout,
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  hello(answer);
+  rl.close();
+});
+
+function hello(answer) {
+  console.log('ff' + answer);
+}
